df225c6cd93c38ce07aa8141341b7c0347d675c8
Pixselve <38262536+Pixselve@users.noreply.github.com>
2022-02-11 10:49:45 +0100

    Initial commit

---
+++ src/lib/CustomInput.svelte
@@ -0,0 +1,20 @@
+<div class="flex space-x-2">
+  <select  class="bg-blue-100 rounded-lg p-2 border border-blue-300 appearance-none after:content-['-'] after:absolute after:top-2 after:right-0" bind:value={value.dropdown}>
+    <option value="🐶">🐶</option>
+    <option value="🐱">🐱</option>
+    <option value="🐭">🐭</option>
+  </select>
+  <input class="bg-blue-100 rounded-lg p-2 border border-blue-300" bind:value={value.input} placeholder="Type here..." type="text">
+  <button on:click={remove}>❌</button>
+</div>
+
+<script lang="ts">
+import { createEventDispatcher } from "svelte";
+
+const dispatch = createEventDispatcher();
+const remove = () => dispatch('remove');
+export let value = {
+  dropdown: "🐶",
+  input: ""
+}
+</script>
---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+import CustomInput from "$lib/CustomInput.svelte";
+
+let valueArray = [];
+
+const addInput = () => {
+  valueArray = [...valueArray, {}]
+};
+
+const removeElement = (index: number) => {
+  valueArray = valueArray.filter((_, i) => i !== index);
+};
+
+</script>
+<div class="space-y-2">
+  {#each valueArray as singleValue, i}
+    <CustomInput on:remove={() => removeElement(i)} bind:value={singleValue}/>
+  {/each}
+  <button class="bg-blue-500 p-2 rounded-lg text-white" on:click={addInput}>✚ Add input</button>
+</div>
+
+
+
+
+{JSON.stringify(valueArray)}
